1d29fc822b02272c9ed597e5c3dd1068c6e4fa9a
Kevin Goebbert <kevin.goebbert@valpo.edu>
2016-08-04 17:00:53 -0500

    Adding an example: arith.for

---
+++ arith.for
@@ -0,0 +1,20 @@
+      PROGRAM arith
+*******************************************************************
+* Kevin Goebbert            MET 330             26 August 2014    *
+*                      arith.for                                  *
+*                                                                 *
+* Program to add two real numbers. Variables used are:            *
+*        X, Y : the two real numbers                              *
+*        SUM  : the sum of X and Y                                *
+*                                                                 *
+* Output:   X, Y, and SUM                                         *
+*******************************************************************
+
+      REAL X, Y, SUM
+
+      X = 3.14
+      Y = 2.057
+      SUM = X + Y
+      PRINT *, 'SUM OF', X, ' AND', Y, ' IS', SUM
+      
+      END
